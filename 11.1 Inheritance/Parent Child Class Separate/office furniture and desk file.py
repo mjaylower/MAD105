5fb2244944904c19c8ff354ee5ca8191c0046e6f
mjaylower <35702869+mjaylower@users.noreply.github.com>
2018-04-03 18:01:34 -0500

    Update office furniture and desk file.py

--- 11.1 Inheritance/Parent Child Class Separate/office furniture and desk file.py
+++ 11.1 Inheritance/Parent Child Class Separate/office furniture and desk file.py
@@ -1,13 +1,20 @@
+# import the parent class officefurniture
+# import the child class desk
+
 import officefurniture
 import desk
 
 
 def main():
+
+    # using new variable pulling from the passed arguments
     chair = officefurniture.OfficeFurniture('Chair', 'Steel', 2, 2, 50)
     desk2 = desk.Desk('Desk', 'Wood', 62, 34, 650, 'Left', 5)
 
+    # print the values passed through the classes to display the class' string
     print(desk2)
     print(chair)
 
 
+# run the program
 main()
