09429cdd02e6e81a63c210c888a68686b6e0a13d
mjaylower <35702869+mjaylower@users.noreply.github.com>
2018-04-03 18:00:16 -0500

    Update inheritance file.py

--- 11.1 Inheritance/Parent Child Class Combo/inheritance file.py
+++ 11.1 Inheritance/Parent Child Class Combo/inheritance file.py
@@ -1,12 +1,18 @@
+# import the parent/child class file
+
 import inheritance
 
 
 def main():
+
+    # create new variable to pass values as arguments to the classes program
     chair = inheritance.OfficeFurniture('Chair', 'Steel', 2, 2, 50)
     desk2 = inheritance.Desk('Desk', 'Wood', 62, 34, 650, 'Left', 5)
 
+    # print the string from each of the class' using the values in the variables as they are passed
     print(desk2)
     print(chair)
 
 
+# run the program
 main()
