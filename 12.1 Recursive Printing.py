c36ece3d8b389aa3932011159166ab46f6d2f085
mjaylower <35702869+mjaylower@users.noreply.github.com>
2018-04-09 11:29:08 -0500

    12.1 Recursive Printing

---
+++ 12.1 Recursive Printing.py
@@ -0,0 +1,15 @@
+# define the main function
+def main():
+    n = int(input('Pick a number: '))  # allow user to enter a number to count up to
+    printing(n)  # call the printing function
+
+
+# define the printing function
+def printing(n):  # pass the value from the user input int
+    if n > 0:  # while the value is greater than zero execute the statements
+        printing(n-1)  # set the recursive function to count down till 0 which will stop the loop
+        print(n)  # print the value
+
+
+# run the main function
+main()
