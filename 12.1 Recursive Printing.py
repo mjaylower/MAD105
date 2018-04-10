8ce5642118ea44c010b2ae48f615627c597dacf1
mjaylower <35702869+mjaylower@users.noreply.github.com>
2018-04-10 10:01:39 -0500

    Update 12.1 Recursive Printing.py

--- 12.1 Recursive Printing.py
+++ 12.1 Recursive Printing.py
@@ -1,14 +1,16 @@
-# define the main function
+# def the main function
 def main():
-    n = int(input('Pick a number: '))  # allow user to enter a number to count up to
-    counting(n)  # call the counting function
+    num_list = [2, 4, 6, 8, 10, 12, 14]  # create the list to be summed
+    list_sum = range_sum(num_list, 0, 6)  # select the list and range to be summed for function 'range_sum'
+    print(list_sum)  # print the summation of the list 'num_list'
 
 
-# define the counting function
-def counting(n):  # pass the value from the user input int
-    if n > 0:  # while the value is greater than zero execute the statements
-        counting(n-1)  # set the recursive function to count down till 0 which will stop the loop
-        print(n)  # print the value
+# def range_sum
+def range_sum(num_list, start, end):  # pass the values from the list, the starting and ending values
+    if start > end:  # if the start number is greater than the end, return 0
+        return 0
+    else:
+        return num_list[start] + range_sum(num_list, start + 1, end)  # start with first values in list add to next value through ending value for each iteration/recursion
 
 
 # run the main function
