71c3214c72db85768d1d606b454a6c5e6fe8167b
mjaylower <35702869+mjaylower@users.noreply.github.com>
2018-04-10 10:03:40 -0500

    Update 12.2 Recursive List Sum.py

--- 12.2 Recursive List Sum.py
+++ 12.2 Recursive List Sum.py
@@ -1,14 +1,17 @@
+# def the main function
 def main():
-    num_list = [2, 4, 6, 8, 10, 12, 14]
-    list_sum = range_sum(num_list, 0, 6)
-    print(list_sum)
+    num_list = [2, 4, 6, 8, 10, 12, 14]  # create the list to be summed
+    list_sum = range_sum(num_list, 0, 6)  # select the list and range to be summed for function 'range_sum'
+    print(list_sum)  # print the summation of the list 'num_list'
 
 
-def range_sum(num_list, start, end):
-    if start > end:
+# def range_sum
+def range_sum(num_list, start, end):  # pass the values from the list, the starting and ending values
+    if start > end:  # if the start number is greater than the end, return 0
         return 0
     else:
-        return num_list[start] + range_sum(num_list, start + 1, end)
+        return num_list[start] + range_sum(num_list, start + 1, end)  # start with first values in list add to next value through ending value for each iteration/recursion
 
 
+# run the main function
 main()
